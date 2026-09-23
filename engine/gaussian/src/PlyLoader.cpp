11f71abe93312d5cb0d90a438d5d218bd93ab3d5
SWAYAM SINGAL <swayam.singal@gmail.com>
2026-09-23 08:53:36 +0530

    style: format compact scalar enum

--- engine/gaussian/src/PlyLoader.cpp
+++ engine/gaussian/src/PlyLoader.cpp
@@ -18,7 +18,16 @@ namespace aether::gaussian {
 namespace {
 
 enum class Format : std::uint8_t { ascii, binaryLittleEndian };
-enum class ScalarType : std::uint8_t { int8, uint8, int16, uint16, int32, uint32, float32, float64 };
+enum class ScalarType : std::uint8_t {
+    int8,
+    uint8,
+    int16,
+    uint16,
+    int32,
+    uint32,
+    float32,
+    float64
+};
 
 struct Property final {
     ScalarType type;
